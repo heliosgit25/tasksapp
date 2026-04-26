deedcfc38d011f4f9fc0a2982634861ca8372854
heliosgit <twishampatighosh1@gmail.com>
2026-04-27 01:56:01 +0530

    Created tasks list, task dict, added inputs and printed

--- tasks.py
+++ tasks.py
@@ -1,1 +1,28 @@
 print("This is a tasks app on Python.")
+
+#dict concept used here for creating a task object
+##task = {
+#    "title": "Learn Python",
+#    "done": False
+#}
+# a tasks list can store multiple task objects
+
+tasks = []
+title = input("What do you need to do?: ")
+comp = int(input("Is it done? Type 0 if not done, or type 1 if done: "))
+priority = input("What's the priority of this task? Enter low, med, high: ")
+
+task = {
+    "title": title,
+    "done": False if comp == 0 else "True",
+    "priority": priority
+}
+
+tasks.append(task)
+
+for i, task in enumerate(tasks, 1):
+    status = "✔" if task["done"] else "✘"
+    print(f"{i}. [{status}] {task['title']} {task['priority']}")
+
+#print(task)
+#print(tasks)
